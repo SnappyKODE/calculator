7c50d8ae34f8364c8e31ce7b49fc53fbadf055dc
Kartikey Khanna <135053308+SnappyKODE@users.noreply.github.com>
2023-12-12 21:33:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ let screen = document.querySelector("h1")
 let v = "";
 var a=0,b = 0,c=1,operator;
 
+function printReset(op){
+    a=0;
+    v="";
+    screen.innerHTML=op;
+    operator=op;
+}
+
 btn.forEach(element => {
     element.addEventListener("click",()=>{
         if(element.value=="reset"){
+            a=b;
+            b=0;
+            c=1;
             v="";
             screen.innerHTML=v;
         }
@@ -19,81 +29,53 @@ btn.forEach(element => {
         }
         else if(element.value == "+"){
             b=b+a;
-            a=0;
-            v="";
-            // console.log(element.value);
-            // console.log(b);
-            screen.innerHTML="+";
-            operator="+";
-
+            printReset(element.value);
         }
         else if(element.value == "-"){
             b=Math.abs(b-a);
-            a=0;
-            v="";
-            // console.log(element.value);
-            // console.log(b);
-            screen.innerHTML="-";
-            operator="-";
+            printReset(element.value);
         }
         else if(element.value == "*"){
             c=Math.abs(c*a);
             b=c;
-            a=0;
-            v="";
-            // console.log(element.value);
-            // console.log(b);
-            screen.innerHTML="x";
-            operator="*";
+            printReset(element.value);
         }
         else if(element.value == "/"){
             c= c>1 ? Math.abs(c/a) : a;
             b=c;
-            a=0;
-            v="";
-            // console.log(element.value);
-            // console.log(b);
-            screen.innerHTML="/";
-            operator="/";
+            printReset(element.value);
         }
         else if(element.value == "equal"){
-            // console.log(b);
-            // screen.innerHTML=b;
 
             if(operator == "+"){
                 b=b+a;
-                // console.log(b);
                 screen.innerHTML=b;
                 operator=""
             }
             if(operator == "-"){
                 b=Math.abs(b-a);
-                // console.log(b);
                 screen.innerHTML=b;
                 operator=""
             }
             if(operator == "*"){
                 c=Math.abs(c*a);
                 b=c;
-                // console.log(b);
                 screen.innerHTML=b;
                 operator=""
             }
             if(operator == "/"){
                 c= c>1 ? Math.abs(c/a) : a;
                 b=c;
-                // console.log(b);
                 screen.innerHTML=b;
                 operator=""
             }
             a=b;
             b=0;
             c=1;
-            v=""
-            // screen.innerHTML=b;
+            v="";
+
         }
         else{
-            // console.log(element.value);
             v = v+element.value;
             a=Number(v);
             screen.innerHTML=v;
@@ -102,7 +84,3 @@ btn.forEach(element => {
     })
 
 });
-
-
-
-
